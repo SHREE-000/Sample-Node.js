04e353e4787773a77bf968b1a394edc1b127e742
Shreehari C R <shreehari000@gamil.com>
2023-09-17 12:00:25 +0530

    Created new sample node js project

---
+++ index.js
@@ -0,0 +1,25 @@
+import express from 'express';
+import cors from 'cors';
+import bodyParser from 'body-parser';
+import { config } from 'dotenv';
+config();
+
+const app = express();
+
+// Middleware
+app.use(express.json({limit: '50mb'}));      
+app.use(express.urlencoded({limit: '50mb', extended: true}));
+app.use(bodyParser.json({limit: '50mb'}));
+app.use(bodyParser.urlencoded({limit: '50mb', extended: true}));
+app.use(cors());
+
+// Routes
+app.get('/', (req, res) => {
+  res.send('Hello, world!');
+});
+
+// Start server
+const port = process.env.PORT
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
+});
